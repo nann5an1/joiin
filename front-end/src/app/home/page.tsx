3dbe7f788f6368bc79aab86dd82430cfa0b21369
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2025-07-07 13:28:36 +0800

    landing_page

--- front-end/src/app/home/page.tsx
+++ front-end/src/app/home/page.tsx
@@ -1,8 +1,13 @@
 'use client'
+import {useRouter} from 'next/navigation';
+import {useEffect} from "react";
 
 export const dynamic = 'force-dynamic';
 export default function Home() {
-    
+    const router = useRouter();
+    useEffect(() => {
+        router.refresh();
+    }, [])
     return (
         <>
             <h1>User's Home Page</h1>
